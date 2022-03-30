f200cce4c8724d60bed2c5c97fbbf59117ad8126
“Danielle <danielle.msousa@hotmail.com>
2022-03-29 21:18:46 -0300

    segundo commit

---
+++ cypress/integration/precadastro.spec.js
@@ -0,0 +1,27 @@
+/// <reference types ="cypress" />
+var faker = require ('faker');
+const Faker = require('faker/lib');
+
+describe('Funcionalidade Pré Cadastro', () => {
+    
+    beforeEach(() => {
+        cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/")
+    });
+    afterEach(() => {
+        cy.screenshot()
+        
+    });
+    
+
+    it('Deve completar o pré cadastro com sucesso para acessar o site da Loja EBAC', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('teste@teste01!')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.name.firstName())
+        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('.woocommerce-Button').click()
+
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso')
+    });
+});
