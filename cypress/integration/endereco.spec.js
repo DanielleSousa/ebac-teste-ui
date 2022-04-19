a4710a4532d35828894a8e114a6d8726899d4521
“Danielle <danielle.msousa@hotmail.com>
2022-04-19 17:33:39 -0300

    usando page object

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,20 +1,17 @@
 /// <reference types ="cypress" />
 
-describe('Funcionalidade Endreços - Faturamento e Entrega', () => {
+import EnderecoPage from '..//support//page-objects/endereco.page'
 
-    beforeEach(() => {
-        cy.visit('minha-conta')
-        cy.fixture('perfil').then(dados=> {
+describe('Funcionalidade Endereços- Faturamento e Entrega', () => {
+beforeEach(() => {
+    cy.visit('minha-conta')
+    cy.fixture('perfil').then(dados =>{
         cy.login(dados.usuario, dados.senha)
-        })
-      
-    });
 
+    })
+});
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-
-        
-        //cadastro de endereço
-        
+        EnderecoPage.editarEnderecoFaturamento('Danielle', 'Monique', 'EBACSUPERSHOP', 'Brasil', 'Afonso Guimarães', '2022', 'Campinas', 'São Paulo', '06435000', '987654321', 'dani_monique@ebac.com.br')
+        cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso.')
     });
-    
 });
